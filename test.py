21b403e7a80d257c5d8d67655d7d45e46635f9ab
Sreeram <SREERAM@LAPTOP-EVHH4L94.>
2024-05-11 20:36:20 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,25 @@
+from mtcnn import MTCNN
+import cv2
+import numpy as np
+
+
+# face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# img = cv2.cvtColor(cv2.imread("fiona_28.jpg"), cv2.COLOR_BGR2RGB)
+# detector = MTCNN()
+# lst=[]
+# lst.append(detector.detect_faces(img))
+
+# img = cv2.cvtColor(cv2.imread("riona_55.jpg"), cv2.COLOR_BGR2RGB)
+# # detector = MTCNN()
+# lst.append(detector.detect_faces(img))
+
+# # gray = cv2.cvtColor('images.jpeg', cv2.COLOR_BGR2GRAY)
+# # face_points = face_detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
+
+# # print(face_points)
+
+# for l in lst:
+#     print(l)
+
+print(np.array('riona_55.jpg'))
